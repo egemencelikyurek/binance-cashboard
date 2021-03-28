bbceaeede4c95c22410e04c2b4bbb24024a2636f
egemencelikyurek <egemencelikyurek@gmail.com>
2021-03-28 19:29:02 +0300

    responsive design

--- public/javascripts/pricetosell.js
+++ public/javascripts/pricetosell.js
@@ -1,8 +1,8 @@
 var pricetosell = document.querySelector("#pricetosell");
 var avgCost = document.querySelector("#avgcost");
-
+pricetosell.value = (parseFloat(avgCost.innerText) * (1 + parseFloat(document.querySelector("#percent").value/100))).toFixed(6);
 document.querySelector("#percent").addEventListener("change", myFunction);
 function myFunction(){
-    pricetosell.textContent = (parseFloat(avgCost.innerText) * (1 + parseFloat(document.querySelector("#percent").value/100))).toFixed(6);
+    pricetosell.value = (parseFloat(avgCost.innerText) * (1 + parseFloat(document.querySelector("#percent").value/100))).toFixed(6);
 } 
 
